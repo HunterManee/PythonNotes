ea9b0abd2a34e62d47445735b47ebaa83c063c63
huntermanee <HunterManee3A33@outlook.com>
2024-09-13 02:53:19 -0400

    9.1-9.2

---
+++ Module9_NestedLoopsAndFileReading.py
@@ -0,0 +1,18 @@
+###### 9.1 ############## Developing Programs Incrementally #################
+'''
+Incremental Programming: Experienced programmers start with a simple version
+    of the program, and grwoing the program little by little into a complete
+    version
+
+FIXME Comment: attracts attention to code that needs to be fixed in the 
+    future
+
+'''
+###### 9.2 ############## Nested Loops ######################################
+'''
+Nested Loop: A loop that appears as part of the body of another loop
+
+Outer Loop: Parent Loop
+
+Inner Loop: Loop that appears inside the Parent Loop
+'''
